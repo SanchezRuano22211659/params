a8f9eb5f3fef606b19b4830a84518dace62c68c2
Alexis Sánchez <126355162+SanchezRuano22211659@users.noreply.github.com>
2023-03-09 20:21:59 +0000

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	new file:   paramsProg/.vscode/launch.json 	new file:   paramsProg/.vscode/tasks.json 	new file:   paramsProg/Program.cs 	new file:   paramsProg/paramsProg.csproj

---
+++ paramsProg/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    private void increases(ref int x){x++;}
+
+    private static void Main(string[] args)
+    {
+        Program p = new Program();
+        int x=10;
+        p.increases(ref x);
+        Console.WriteLine(x);
+    }
+}
