01d9af7e7a6ad8afb00c44474aa9acee88608ff7
Alexis Sánchez <126355162+SanchezRuano22211659@users.noreply.github.com>
2023-03-09 20:55:56 +0000

    modified:   paramsProg/Program.cs

--- paramsProg/Program.cs
+++ paramsProg/Program.cs
@@ -1,12 +1,16 @@
 ﻿class Program
 {
-    private void increases(ref int x){x++;}
-
+    private static void increases(ref int x){x++;}
+    private static void plus(int a, int b, out int c, out int d){c = a+b; d = 2;}
     private static void Main(string[] args)
     {
-        Program p = new Program();
         int x=10;
-        p.increases(ref x);
+        int y=10;
+        int resultado, resultado2;
+        increases(ref x);
+        plus(x,y,out resultado, out resultado2);
         Console.WriteLine(x);
+        Console.WriteLine(resultado);
+        Console.WriteLine(resultado2);
     }
 }
